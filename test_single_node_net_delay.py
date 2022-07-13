2d136b935e06d3096cde1de674ee8e8c01d3a123
huomax <630509357@qq.com>
2022-07-13 13:53:38 +0800

    add test raspbian single node server delta

--- test_single_node_net_delay.py
+++ test_single_node_net_delay.py
@@ -12,9 +12,9 @@ import cv2
 
 # constant
 time_delta_test_rounds = 1000
-server_node = 'ubantu21'
+server_node = 'raspbian4-old'
 writer_path = server_node + '_single_node_net_delay.xlsx'
-grpc_ip = '192.168.40.133'
+grpc_ip = '192.168.31.187'
 grpc_port = 10000
 img_path = './test_grpc.jpg'
 
