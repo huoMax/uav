66080b54751b4f1158422f2560fba1b5f67e8a10
huomax <630509357@qq.com>
2022-07-14 09:41:28 +0800

    add gradully test data

--- test_gradully_net_delay.py
+++ test_gradully_net_delay.py
@@ -21,17 +21,18 @@ img_path = './test_grpc.jpg'
 if __name__ == "__main__":
     img = cv2.imread(img_path)
     str_encode = ImgEncode(img, ".jpg")
+    writer = pd.ExcelWriter(writer_path)
 
     # get grpc client
     client_stub = GetClientStub(grpc_ip, grpc_port)
     time_data = {}
+    average_time = []
 
     for i in range(1, 21):
         max_length = int(len(str_encode) / 20) * i
         img_orig = str_encode[:max_length]
+        print("No."+str(i)+": "+str(time.time()))
         for test_round in range(1, test_rounds + 1):
-            if test_round % 100 == 0:
-                print('网络延迟测试：' + str(test_round / 100) + ": " + str(time.time()))
             start_request_time = time.time()
             arrival_time = float(client_stub.server_time_delta(
                 task_pb2.FaceRecognitionRequest(sequence=0, img_orig=img_orig, target="wgk")).arrival_time)
@@ -42,10 +43,11 @@ if __name__ == "__main__":
             time_data[test_round]['start_request_time'] = start_request_time
             time_data[test_round]['arrival_time'] = arrival_time
             time_data[test_round]['end_request_time'] = end_request_time
-
-    df = pd.DataFrame(data=time_data).T
-    df['absolute_net_delay'] = (df['end_request_time'] - df['start_request_time']) / 2
-    writer = pd.ExcelWriter(writer_path)
-    df.to_excel(writer, index=False)
+        df = pd.DataFrame(data=time_data).T
+        df['absolute_net_delay'] = (df['end_request_time'] - df['start_request_time']) / 2
+        average_time.append(df['absolute_net_delay'].mean())
+        df.to_excel(writer, index=False, sheet_name=str(i))
+        time_data = {}
+    print(average_time)
     writer.save()
     writer.close()
